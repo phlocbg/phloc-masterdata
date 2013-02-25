ee8ab64496205d4fb853b2bad951da1333e4a262
ph <ph@phloc.com>
2013-02-25 09:40:13 +0000

    Some cleansing

---
+++ src/main/java/com/phloc/masterdata/email/validation/StringEmailAddressValidator.java
@@ -0,0 +1,33 @@
+package com.phloc.masterdata.email.validation;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import com.phloc.validation.EStandardValidationErrorTexts;
+import com.phloc.validation.result.IValidationResult;
+import com.phloc.validation.result.ValidationResultError;
+import com.phloc.validation.result.ValidationResultSuccess;
+import com.phloc.validation.validator.string.AbstractStringValidator;
+import com.phloc.validation.validator.string.StringEmailAddressNoMXCheckValidator;
+import com.phloc.web.email.EmailAddressValidator;
+
+/**
+ * Validator for email addresses. Uses the {@link EmailAddressValidator} class.<br>
+ * Warning: this may be slow if the MX record check is enabled because DNS
+ * lookups take place
+ * 
+ * @see EmailAddressValidator
+ * @see StringEmailAddressNoMXCheckValidator
+ * @see StringEmailAddressWithMXCheckValidator
+ * @author philip
+ */
+public final class StringEmailAddressValidator extends AbstractStringValidator
+{
+  @Nonnull
+  public IValidationResult validate (@Nullable final String sValue)
+  {
+    if (EmailAddressValidator.isValid (sValue))
+      return ValidationResultSuccess.getInstance ();
+    return new ValidationResultError (EStandardValidationErrorTexts.INVALID_EMAIL_ADDRESS);
+  }
+}
