ca88e28716aa37b5f2214e8a9ec9ab61fab0c003
ph@phloc.com <ph@phloc.com@c3717ffa-ba18-09c0-ffce-88c57dceb524>
2014-01-02 10:33:11 +0000

    [maven-release-plugin]  copy for tag phloc-masterdata-3.3.5

---
+++ tags/phloc-masterdata-3.3.5/src/main/java/com/phloc/masterdata/currency/ECurrencyName.java
@@ -0,0 +1,77 @@
+/**
+ * Copyright (C) 2006-2014 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phloc.masterdata.currency;
+
+import java.util.Locale;
+
+import javax.annotation.Nonnull;
+
+import com.phloc.commons.annotations.DevelopersNote;
+import com.phloc.commons.annotations.Translatable;
+import com.phloc.commons.name.IHasDisplayText;
+import com.phloc.commons.text.ITextProvider;
+import com.phloc.commons.text.impl.TextProvider;
+import com.phloc.commons.text.resolve.DefaultTextResolver;
+
+@Translatable
+public enum ECurrencyName implements IHasDisplayText
+{
+  ALL ("Albanische Lek", "Albanian Lek"),
+  AMD ("Armenische Dram", "Armenian Dram"),
+  AZN ("Aserbaidschanische Manat", "Azerbaijani Manat"),
+  BAM ("Konvertible Marks", "Bosnia-Herzegovina Convertible Mark"),
+  BGN ("Bulgarische Leva", "Bulgarian Lev"),
+  BYR ("Weißrussische Rubel", "Belarusian Ruble"),
+  CHF ("Schweizer Franken", "Swiss Franc"),
+  CZK ("Tschechische Kronen", "Czech Koruna"),
+  DKK ("Dänische Kronen", "Danish Krone"),
+  @Deprecated
+  @DevelopersNote ("Was replaced with Euro per 1.1.2011")
+  EEK ("Estnische Krone", "Estonian Kroon"),
+  EUR ("Euro", "Euro"),
+  GBP ("Pfund Sterling", "Pound Sterling"),
+  GEL ("Georgische Lari", "Georgian Lari"),
+  HRK ("Kroatische Kuna", "Croatian Kuna"),
+  HUF ("Ungarische Forint", "Hungarian Forint"),
+  ISK ("Isländische Krone", "Icelandic Krona"),
+  LTL ("Litauische Litas", "Lithuanian Litas"),
+  LVL ("Lettische Lats", "Latvian Lats"),
+  MDL ("Moldawische Leu", "Moldovan Leu"),
+  MKD ("Mazedonische Dinar", "Macedonia Denar"),
+  NOK ("Norwegische Krone", "Norwegian Krone"),
+  PLN ("Polnische Złoty", "Polish Złoty"),
+  RON ("Rumänische Leu", "Romanian Leu"),
+  RSD ("Serbische Dinar", "Serbian dinar"),
+  RUB ("Russische Rubel", "Russian Ruble"),
+  SEK ("Schwedische Kronen", "Swedish Krona"),
+  TRY ("Türkische Lira", "New Turkish Lira"),
+  UAH ("Ukrainische Hrywnja", "Ukrainian Hryvnia"),
+  USD ("US Dollar", "United States Dollar");
+
+  private final ITextProvider m_aTP;
+
+  private ECurrencyName (@Nonnull final String sDE, @Nonnull final String sEN)
+  {
+    m_aTP = TextProvider.create_DE_EN (sDE, sEN);
+  }
+
+  public String getDisplayText (@Nonnull final Locale aContentLocale)
+  {
+    return DefaultTextResolver.getText (this, m_aTP, aContentLocale);
+  }
+}
---
+++ tags/phloc-masterdata-3.3.5/src/test/java/com/phloc/masterdata/currency/CurrencyValueTest.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (C) 2006-2014 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phloc.masterdata.currency;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.math.BigDecimal;
+
+import org.junit.Test;
+
+import com.phloc.commons.charset.CSpecialChars;
+import com.phloc.commons.mock.AbstractPhlocTestCase;
+import com.phloc.commons.mock.PhlocTestUtils;
+
+public final class CurrencyValueTest extends AbstractPhlocTestCase
+{
+  @Test
+  public void testGetFormatted ()
+  {
+    ICurrencyValue aCV = new CurrencyValue (ECurrency.EUR, new BigDecimal (5));
+    assertEquals (CSpecialChars.EURO_STR + " 5,00", aCV.getCurrencyFormatted ());
+    aCV = new CurrencyValue (ECurrency.EUR, new BigDecimal ("5.12"));
+    assertEquals (CSpecialChars.EURO_STR + " 5,12", aCV.getCurrencyFormatted ());
+    aCV = new CurrencyValue (ECurrency.USD, new BigDecimal ("5.12"));
+    assertEquals ("$5.12", aCV.getCurrencyFormatted ());
+
+    for (final ECurrency eCurrency : ECurrency.values ())
+    {
+      aCV = new CurrencyValue (eCurrency, new BigDecimal ("5.12"));
+      assertNotNull (aCV.getCurrencyFormatted ());
+      assertNotNull (aCV.getValueFormatted ());
+      PhlocTestUtils.testGetClone (aCV);
+
+      m_aLogger.info ("[" + aCV.getCurrencyFormatted () + "][" + aCV.getValueFormatted () + "]");
+    }
+  }
+}
