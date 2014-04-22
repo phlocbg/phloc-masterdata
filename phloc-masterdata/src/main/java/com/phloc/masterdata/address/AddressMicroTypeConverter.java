94f6b4de6266d833d491fe750bff85ef865962ca
ph <ph@phloc.com>
2014-04-22 11:07:29 +0000

    Using named classes for micro type converters

---
+++ phloc-masterdata/src/main/java/com/phloc/masterdata/address/AddressMicroTypeConverter.java
@@ -0,0 +1,26 @@
+package com.phloc.masterdata.address;
+
+import java.util.Locale;
+
+import javax.annotation.Nonnull;
+
+import com.phloc.commons.microdom.IMicroElement;
+import com.phloc.commons.system.SystemHelper;
+
+public class AddressMicroTypeConverter extends AbstractAddressMicroTypeConverter
+{
+  @Nonnull
+  public Address convertToNative (@Nonnull final IMicroElement eAddress)
+  {
+    final Locale aLocale = SystemHelper.getSystemLocale ();
+    final EAddressType eType = EAddressType.getFromIDOrNull (eAddress.getAttribute (ATTR_TYPE));
+    final String sCountry = eAddress.getAttribute (ATTR_COUNTRY);
+    final String sState = eAddress.getAttribute (ATTR_STATE);
+    final String sPostalCode = eAddress.getAttribute (ATTR_POSTALCODE);
+    final String sCity = eAddress.getAttribute (ATTR_CITY);
+    final String sStreet = eAddress.getAttribute (ATTR_STREET);
+    final String sBuildingNumber = eAddress.getAttribute (ATTR_BUILDINGNUMBER);
+    final String sPostOfficeBox = eAddress.getAttribute (ATTR_POBOX);
+    return new Address (eType, sCountry, sState, sPostalCode, sCity, sStreet, sBuildingNumber, sPostOfficeBox, aLocale);
+  }
+}
